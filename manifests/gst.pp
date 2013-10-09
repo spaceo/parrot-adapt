4f0663090597b2ab750d83f22408b53b6192e491
Mikkel <mikkel@adapt.dk>
2013-10-10 00:10:31 +0200

    Adding drush to gst manifest

--- manifests/gst.pp
+++ manifests/gst.pp
@@ -6,7 +6,19 @@ exec { "apt-update":
 }
 
 Exec["apt-update"] -> Package <| |>
-
+  
+  class {'pear': }
+  
+  exec { "pear update-channels" :
+    command => "/usr/bin/pear update-channels",
+    require => [Package['php-pear']]
+  } 
+  exec {"pear install drush":
+    command => "/usr/bin/pear channel-discover pear.drush.org && /usr/bin/pear install drush/drush",
+    creates => '/usr/bin/drush',
+    require => Exec['pear update-channels']
+  }
+ 
   package { 'php5-pgsql': }
  
   class { 'postgresql':
